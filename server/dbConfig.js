40202857af0201595b348a5480ad3825cbee8eb1
Timo Asumaniemi <asumaniemi.timo@outlook.com>
2023-10-23 20:46:40 +0300

    Initial connection
    Initial Database connection to Nodejs server
    Initial select query added to server
    React app supports connection to Node server

---
+++ server/dbConfig.js
@@ -0,0 +1,10 @@
+const dbConfig = {
+    settings: {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'shoppinglist'
+    }
+}
+
+module.exports = dbConfig;
--- server/index.js
+++ server/index.js
@@ -3,13 +3,26 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
-
 const port = 3001
 
+const dbConfig = require('./dbConfig')
+const mysql = require('mysql2/promise')
 
+app.get("/",async (request, response) =>{
+    const queryString = "SELECT * FROM item;"
+    try{
+        console.log("Opening DB Connection with: " + dbConfig.settings.host);
+        const connection = await mysql.createConnection(dbConfig.settings);
+        const [result] = await connection.execute(queryString);
+        
+        if(!result)
+            result = []
 
-app.get("/",(request, response) =>{
-    response.status(200).json({status:1, message: "Default api ok"})
+        response.status(200).json({status:1, message: "Default api ok", data:result[0]})
+    } catch(error){
+        console.log(error.message)
+        response.status(500).json({status:-1, message:error.message})
+    }
 });
 
 app.listen(port, () =>{
