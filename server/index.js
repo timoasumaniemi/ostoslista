0986bbaeb9d0c7e557c42fc5648b8fd270be6d5f
Timo Asumaniemi <asumaniemi.timo@outlook.com>
2023-10-24 09:58:58 +0300

    Refactoring and UI updates
    React ui shows list of items in db
    Node server refactoring and cleanup

--- server/index.js
+++ server/index.js
@@ -1,30 +1,30 @@
 const express = require('express')
 const cors = require('cors')
+const dbConfig = require('./dbConfig')
+const mysql = require('mysql2/promise')
 
 const app = express()
 app.use(cors())
 const port = 3001
+const query_selectAllItems = "SELECT * FROM item;"
 
-const dbConfig = require('./dbConfig')
-const mysql = require('mysql2/promise')
-
-app.get("/",async (request, response) =>{
-    const queryString = "SELECT * FROM item;"
-    try{
-        console.log("Opening DB Connection with: " + dbConfig.settings.host);
-        const connection = await mysql.createConnection(dbConfig.settings);
-        const [result] = await connection.execute(queryString);
-        
-        if(!result)
-            result = []
-
-        response.status(200).json({status:1, message: "Default api ok", data:result[0]})
-    } catch(error){
-        console.log(error.message)
-        response.status(500).json({status:-1, message:error.message})
-    }
+app.get("/", async (request, response) => {
+    response.status(200).json({ message: `To see shoppinglist content, use '${dbConfig.settings.host}:${port}/api/shoppinglist' ` })
 });
 
-app.listen(port, () =>{
+app.get("/api/shoppinglist", async (request, response) => {
+
+    try {
+        const connection = await mysql.createConnection(dbConfig.settings);
+        const [result] = await connection.execute(query_selectAllItems);
+
+        response.status(200).json(result)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).json({ message: error.message })
+    }
+})
+
+app.listen(port, () => {
     console.log(`Server is running at port ${port}`)
 })
--- src/App.js
+++ src/App.js
@@ -2,33 +2,40 @@ import './App.css';
 import axios from 'axios';
 import { useState } from 'react';
 
-const url = "http://localhost:3001"
+const url = "http://localhost:3001/api/shoppinglist"
 
 function App() {
-const [data, setData] = useState("")
+  const [items, setItems] = useState([])
 
-  function FetchData(e){
+  function FetchData(e) {
     e.preventDefault()
     console.log("Fetching data")
 
     axios.get(url).
-    then(response => {
-      console.log("Data: id=" + response.data.data.id + " : description= " + response.data.data.description + " : amount= " + response.data.data.amount)
-      setData(response.data.message)
-    }).catch(error => {
-      console.log(error)
-      alert(error.response.data.message)
-    })
+      then(response => {
+        if (!response.data) {
+          throw ("no content: " + response.data.message)
+        }
+
+        setItems(response.data)
+
+      }).catch(error => {
+        console.log(error)
+        alert(error)
+      })
   }
 
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Ostolista v1.0</h1>
-      <div>
-        <button onClick={(e) =>{ FetchData(e)} }>Fetch</button>
-        <p>{data}</p>
-      </div>
+        <h1>Ostolista v1.0</h1>
+        <div>
+          <button onClick={(e) => { FetchData(e) }}>Fetch</button>
+          <ol>{items.map(item => (
+            <li key={item.id}>{item.description}, Amount: {item.amount}</li>
+          ))}
+          </ol>
+        </div>
       </header>
     </div>
   );
